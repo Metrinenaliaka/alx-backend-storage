5196b2ee48cd1b27375c80e99c2b83e46634e8ba
Metrinenaliaka <metrinenam@gmail.com>
2024-05-09 23:19:54 +0300

    fixed errors

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,11 +1,10 @@
--- Create a temporary table to hold the rankings
-CREATE TEMPORARY TABLE temp_rankings AS
+-- Create a temporary table to store the aggregated fan counts per country
+CREATE TEMPORARY TABLE temp_fan_counts AS
 SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
 GROUP BY origin;
 
--- Rank the origins based on the number of non-unique fans
-SELECT origin, nb_fans,
-       DENSE_RANK() OVER (ORDER BY nb_fans DESC) AS ranking
-FROM temp_rankings
-ORDER BY ranking;
+-- Rank the country origins based on the number of fans
+SELECT origin, nb_fans
+FROM temp_fan_counts
+ORDER BY nb_fans DESC;
