7926b26055a91616b1c442681d87050229a780e6
Metrinenaliaka <metrinenam@gmail.com>
2024-05-09 14:54:05 +0300

    fixed errors

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 IF NOT EXISTS (SELECT * FROM information_schema.tables WHERE table_name = 'users') THEN
     -- Create the table users if it does not exist
     CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255)
     );
